cbeb3a6e454f8c9b8e426eff5849cc90730a872e
Nivetha <nivethajega26@gmail.com>
2023-07-06 01:43:45 +0530

    .

---
+++ SmartBuy.API/SmartBuy.Core/KnownString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartBuy.Core
+{
+    public class KnownString
+    {
+        public const string ConnectionName = "MyConnection";
+
+    }
+}
---
+++ SmartBuy.API/SmartBuy.Infrastructure/DesignTimeDbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartBuy.Core;
+
+namespace SmartBuy.Infrastructure
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            //var builder = new ConfigurationBuilder()
+            //              .SetBasePath(Directory.GetCurrentDirectory())
+            //              .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            //var Configuration = builder.Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=SmartBuyDB;Integrated Security=True");
+
+            //optionsBuilder.UseSqlServer(Configuration.GetConnectionString(KnownString.ConnectionName));
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
