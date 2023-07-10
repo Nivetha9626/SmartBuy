f46e15280cc772fe0c5d20c3bc086a58c5c4730d
Nivetha <nivethajega26@gmail.com>
2023-07-10 11:21:37 +0530

    .

--- SmartBuyUI/src/app/models/CustomerDto.ts
+++
@@ -1,9 +0,0 @@
-import { BaseIdAuditEntity } from "./BaseIdAuditEntity";
-
-export class CustomerDto extends BaseIdAuditEntity {
-    firstName: string;
-    lastName: string;
-    dob: Date;
-    email: string;
-    address: string;
-}
